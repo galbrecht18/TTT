5b06764780454248506f75ffbbeb7a2833fe0243
galbrecht18 <galbrecht18@gmail.com>
2015-09-08 20:42:59 -0400

    Updated version per comments, working now.

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -7,10 +7,8 @@
 
 require 'pry'
 
-#constant of winning lines
 WINNING_LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [7,5,3], [1,5,9]]
 
-#create board hash
 def create_board_hash
 
   board = {}
@@ -19,7 +17,6 @@ def create_board_hash
 
 end
 
-#method to visually draw the board
 def draw_board(board)
   puts "#{board[1]}|#{board[2]}|#{board[3]}"
   puts "-----"
@@ -28,7 +25,6 @@ def draw_board(board)
   puts "#{board[7]}|#{board[8]}|#{board[9]}"
 end
 
-#method to set icons
 def set_player_icon
 
   puts "Welcome to Tic Tac Toe! Do you want to be X's or O's? (x/o)"
@@ -42,7 +38,6 @@ def set_player_icon
 
 end 
 
-#method to set comp icon
 def set_comp_icon player_icon
 
   if player_icon == 'X'
@@ -57,11 +52,10 @@ end
 
 def find_empty_spaces board
 
-  board.select {|k,v| v == ' '}
+  board.select {|_k,v| v == ' '}
 
 end 
 
-#method to output help
 def print_help
   puts "\nBelow is the board grid. \nSelect the appropriate number for the position you want.\n\n"
   puts "1|2|3"
@@ -71,7 +65,6 @@ def print_help
   puts "7|8|9\n\n"
 end
 
-#method for player picking
 def player_picks player_icon, board
 
   puts "Please select a square (1-9). 1 corresponds to top left, 9 for bottom right.\nType \'help\' for more details."
@@ -100,9 +93,9 @@ def computer_picks board, computer_icon, player_icon
   WINNING_LINES.each do |line|
     if board.values_at(*line).count(player_icon) == 2
       line.each do |item|
-        computer_pick = board.select{|k,v|k == item and v == ' '}.keys.first
+        computer_pick = board.select{|k,v|k == item && v == ' '}.keys.first
         board[computer_pick] = computer_icon
-        return board
+        return board if computer_pick
       end 
     end 
   end 
@@ -111,7 +104,6 @@ def computer_picks board, computer_icon, player_icon
 
 end
 
-#method to check for a winner. Modeled after solution video. 
 def check_for_winner board, player_icon, computer_icon
   
   WINNING_LINES.each do |line|
@@ -122,7 +114,7 @@ def check_for_winner board, player_icon, computer_icon
 
 end
 
-#Start game with welcome message and determine what the player wants to use. Regex input to make sure its x or o
+#Start game with welcome message and determine what the player wants to use. 
 begin
 
   board = create_board_hash
@@ -130,10 +122,10 @@ begin
   #each time theres a new game reset the icons
   begin 
     player_icon = set_player_icon
-    if player_icon != nil
+    if player_icon 
       computer_icon = set_comp_icon(player_icon)
     end 
-  end until player_icon and computer_icon
+  end until player_icon && computer_icon
 
   begin 
     #Get player input. Create a quick help method too. Also regex here. 
@@ -144,10 +136,10 @@ begin
       computer_picks(board, computer_icon, player_icon)
       draw_board(board)
     end 
-    #check for a winner
+
     winner = check_for_winner(board, player_icon, computer_icon)
 
-  end until find_empty_spaces(board).empty? or winner
+  end until find_empty_spaces(board).empty? || winner
 
   if winner 
     puts "#{winner} won!"
